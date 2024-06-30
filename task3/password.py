423aee488818294481d61a64e64e72c5781aef23
aliya025 <shaikaliya889@gmail.com>
2024-06-30 11:48:19 +0530

    Add files via upload

--- task3/password.py
+++ task3/password.py
@@ -1,1 +1,19 @@
+import secrets
+import string
+def create_pw(pw_length):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
+    alphabet = letters + digits + special_chars
+    pwd = ''
+    pw_strong = False
+    while not pw_strong:
+        pwd = ''.join(secrets.choice(alphabet) for i in range(pw_length))
+        if (any(char in special_chars for char in pwd) and
+            sum(char in digits for char in pwd) >= 2):
+            pw_strong = True
+    return pwd
+if __name__ == '__main__':
+    pw_length = int(input("Enter the desired password length: "))
+    print(create_pw(pw_length))
 
